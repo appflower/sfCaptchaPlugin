14594266113a9d53c3826ebcc9e6f14fb0bea434
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2010-09-30 00:47:09 +0000

    #412 - Upgraded to symfony 1.3

--- lib/captchaValidator.class.php
+++ lib/captchaValidator.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class captchaValidator extends sfValidator {
+class captchaValidator extends sfValidatorBase {
   public function execute (&$value, &$error) {
     $g = new Captcha(sfContext::getInstance()->getUser()->getAttribute('captcha'));
     if ($g->verify($value)) 
